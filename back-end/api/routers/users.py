dd6d48345cf143a58da6c82d648bb6686d6e9dbd
John Murray <johnmurray@fastmail.com>
2022-12-03 16:56:15 -0500

    add UserOutput model to GET queries

--- back-end/api/routers/users.py
+++ back-end/api/routers/users.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
+from typing import List
 
 from schemas.users import User, UserInput, UserOutput
 
@@ -7,18 +8,18 @@ from database import get_session
 
 prefix = "/users"
 router = APIRouter(
-    prefix=f"{prefix}", tags=[prefix], responses={404: {"description": "Not found"}}
+    prefix=f"{prefix}", tags=[prefix], responses={404: {"description": "Not found."}}
 )
 
 
-@router.get("/")
+@router.get("/", response_model=List[UserOutput])
 def get_users(session: Session = Depends(get_session)):
     """Return a list of users."""
     query = select(User)
     return session.exec(query).all()
 
 
-@router.get("/{id}", response_model=User)
+@router.get("/{id}", response_model=UserOutput)
 def get_user_by_id(id: int, session: Session = Depends(get_session)):
     """Return a specified user."""
     user = session.get(User, id)
