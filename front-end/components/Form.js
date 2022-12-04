388cc23eea3cedb87453d1f291dcbf8d93d34589
John Murray <johnmurray@fastmail.com>
2022-12-04 14:36:15 -0500

    adds bootstrap comps

--- front-end/components/Form.js
+++ front-end/components/Form.js
@@ -1,12 +1,13 @@
 import { useRef, useState } from "react";
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
 
-export default function Form() {
+export default function MyForm() {
   const [message, setMessage] = useState();
 
   const usernameRef = useRef();
   const passwordRef = useRef();
 
-  async function onSubmit(e) {
+  async function handleSubmit(e) {
     e.preventDefault();
     try {
       let req = await fetch("http://localhost:8000/api/users/", {
@@ -30,16 +31,43 @@ export default function Form() {
   }
 
   return (
-    <div className="App">
-      <form onSubmit={onSubmit}>
-        <label htmlFor="username">Username</label>
-        <input ref={usernameRef} type="text" id="username" />
-        <label htmlFor="password">Password</label>
-        <input ref={passwordRef} type="password" id="password" />
-        <button type="submit">Submit</button>
-
-        {/* <div className="message">{message ? <p>{message}</p> : null}</div> */}
-      </form>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Name</Form.Label>
+              <Form.Control
+                required
+                type="text"
+                ref={usernameRef}
+                placeholder="Enter a username."
+              ></Form.Control>
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="formBasicPassword]">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                required
+                type="password"
+                ref={passwordRef}
+              ></Form.Control>
+              <Form.Text className="text-muted">
+                Your secrets are safe.
+              </Form.Text>
+            </Form.Group>
+          </Col>
+        </Row>
+        <Stack direction="horizontal" gap={2} className={"justify-content-end"}>
+          <Button type="submit" variant="primary">
+            Submit
+          </Button>
+          <Button type="button" variant="outline-secondary">
+            Cancel
+          </Button>
+        </Stack>
+      </Stack>
+    </Form>
   );
 }
